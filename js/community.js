35ce0e8826ac011ef7ac590a7dde46359643e513
vinhtb <vinhtb@growly.life>
2026-09-19 10:04:26 +0700

    chore: auto-recover community tunnel URL (watchdog)

--- js/community.js
+++ js/community.js
@@ -19,7 +19,7 @@ const Community = {
   // override for whenever this goes stale before a redeploy catches up.
   DEFAULT_URL: (location.hostname === 'localhost' || location.hostname === '127.0.0.1')
     ? 'http://127.0.0.1:8090'
-    : 'https://imaging-lodge-holdem-warehouse.trycloudflare.com',
+    : 'https://casting-checking-cube-equally.trycloudflare.com',
   get BASE_URL() {
     return (localStorage.getItem('community_api_url') || this.DEFAULT_URL).replace(/\/$/, '');
   },
