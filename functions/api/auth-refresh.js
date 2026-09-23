b65fb6178ceb3b900beaa61261fd701e6eac7d25
vinhtb <vinhtb@growly.life>
2026-09-23 12:08:33 +0700

    chore: auto-recover community tunnel URL (watchdog)

--- functions/api/auth-refresh.js
+++ functions/api/auth-refresh.js
@@ -11,7 +11,7 @@
 // functions/api/. Must be listed in $pagesFunctionsWithPbUrl in both
 // Bat-TOAN-BO*.ps1 scripts.
 
-const PB_URL = 'https://advertisements-contrary-indicating-dev.trycloudflare.com';
+const PB_URL = 'https://finish-disable-seeing-driving.trycloudflare.com';
 
 function jsonError(status, message) {
   return new Response(JSON.stringify({ message }), {
