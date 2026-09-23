31410c7d0c924371ddfc13bdb10c0e910ea3d420
vinhtb <vinhtb@growly.life>
2026-09-23 11:24:50 +0700

    chore: auto-update community tunnel URL (Bat-TOAN-BO)

--- functions/api/auth-refresh.js
+++ functions/api/auth-refresh.js
@@ -11,7 +11,7 @@
 // functions/api/. Must be listed in $pagesFunctionsWithPbUrl in both
 // Bat-TOAN-BO*.ps1 scripts.
 
-const PB_URL = 'https://efficiency-thoroughly-kenneth-chapel.trycloudflare.com';
+const PB_URL = 'https://cheque-virgin-merit-diversity.trycloudflare.com';
 
 function jsonError(status, message) {
   return new Response(JSON.stringify({ message }), {
