40f8cd303a46c1f7af016fe6639b72d695375b15
vinhtb <vinhtb@growly.life>
2026-09-23 18:34:26 +0700

    chore: auto-recover community tunnel URL (watchdog)

--- functions/api/login.js
+++ functions/api/login.js
@@ -22,7 +22,7 @@
 // $pagesFunctionsWithPbUrl in both scripts or it will drift to a
 // dead tunnel on the next rotation.
 
-const PB_URL = 'https://ambassador-annotation-messages-flyer.trycloudflare.com';
+const PB_URL = 'https://operation-consultation-floating-cigarette.trycloudflare.com';
 
 function jsonError(status, message) {
   return new Response(JSON.stringify({ message }), {
